831ef2f85755050e597660e4bb43ae04e234dc53
Ertinox444 <paulalexandre.pilon@gmail.com>
2024-01-08 18:51:46 +0100

    Mise en place de la liste de carte

---
+++ Class/Event.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace firstMobileApp.Class
+{
+    internal class Event
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ProfilePhoto { get; set; }
+        public string EventPhoto { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
